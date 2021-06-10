b5469e0119e94149b4fe02870878a960670bb7dc
FacundoTissera <facundotisserasorribas@gmail.com>
2021-06-10 09:29:40 -0300

    agregue alumnos a app-v2

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("Suerte en el curso");
+
+
+let alumno = [{
+    
+    nombres: "Facundo",
+    apellido: "Tissera",
+    contacto: 3534813541,
+    localidad: "Villa Maria",
+},
+{ 
+    nombres: "Jorge",
+    apellido: "Herrando",
+    contacto: 156879523,
+    localidad: " "
+}]
+
+
+for (let i = 0; i < alumno.length; i++) {
+    const alumnos = alumno[i];
+    console.log(alumno[i]);
+    console.log(' alumno'  + ' = ' + alumno[i].nombres);
+}
+
