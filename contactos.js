5967213fdc689495c554c61cb0947b95945193d5
FacundoTissera <facundotisserasorribas@gmail.com>
2021-06-09 23:15:04 -0300

    version-v4

--- contactos.js
+++ contactos.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let a = 'ho' ;
 let b = 'la' ;
 let saludar = a + b;
@@ -6,7 +5,5 @@ let saludar = a + b;
 console.log(saludar);
 
 console.log("este es el contacto");
-=======
 console.log("este es el contacto");
 console.log("modificacion Jorge");
->>>>>>> 83addedc66e0fafc7084fae24636eddd6cc53ac2
